dce0b134b8031560b6a5f8f9c9fc24a133b97783
Ruilu Gao <Ruilu.Gao.23@cmc.edu>
2021-04-08 03:05:27 -0700

    Map revision

--- src/map.py
+++ src/map.py
@@ -56,13 +56,16 @@ with zipfile.ZipFile(args.input_path) as archive:
 
                 # convert text to lower case
                 text = tweet['text'].lower()
+                country = tweet['place']['country_code'].lower()
 
                 # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        counter_country[hashtag][country] += 1    
                     counter_lang['_all'][lang] += 1
+                    counter_country['_all'][country] += 1
 
 # open the outputfile
 try:
@@ -76,3 +79,7 @@ print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
 
+output_path_country = output_path_base+'.country'
+print('saving',output_path_country)
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
